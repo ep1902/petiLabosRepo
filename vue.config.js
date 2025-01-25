4b48291c00aa2b35a00b84f70156f58a6f08b60a
ep1902 <148714834+ep1902@users.noreply.github.com>
2025-01-25 14:58:00 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,3 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new (require("webpack").DefinePlugin)({
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false",
-      }),
-    ],
-  },
-};
